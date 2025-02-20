fc23a819e4c0a3bd4d9568612b98787e7c020a3d
anonymze <anodevfr@gmail.com>
2025-02-20 18:14:49 +0100

    add: react query + states app refetch

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,34 @@
 import "react-native-reanimated";
 import "@/styles/app.css";
 
+import { Text, View, Pressable, Platform, AppState, AppStateStatus } from "react-native";
 import { GestureHandlerRootView, TextInput } from "react-native-gesture-handler";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { Text, View, Pressable, Platform } from "react-native";
+import { onlineManager } from "@tanstack/react-query";
 import { SimpleLineIcons } from "@expo/vector-icons";
+import { focusManager } from "@tanstack/react-query";
 import * as SplashScreen from "expo-splash-screen";
 import { Link, router, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as Network from "expo-network";
 import { useFonts } from "expo-font";
 import React from "react";
 
+
+// refetch on network change
+onlineManager.setEventListener((setOnline) => {
+	const eventSubscription = Network.addNetworkStateListener((state) => {
+		setOnline(!!state.isConnected);
+	});
+	return eventSubscription.remove;
+});
+
+// refetch on app focus
+function onAppStateChange(status: AppStateStatus) {
+	if (Platform.OS !== "web") {
+		focusManager.setFocused(status === "active");
+	}
+}
 
 export default function RootLayout() {
 	const [loaded] = useFonts({
@@ -28,6 +46,13 @@ export default function RootLayout() {
 	if (!loaded) {
 		return null;
 	}
+
+	// refetch on app focus
+	React.useEffect(() => {
+		const subscription = AppState.addEventListener("change", onAppStateChange);
+		return () => subscription.remove();
+	}, []);
+
 	return (
 		<GestureHandlerRootView>
 			<StatusBar style="light" translucent />
@@ -36,7 +61,7 @@ export default function RootLayout() {
 					<Stack
 						screenOptions={{
 							headerShown: false,
-							animation: Platform.OS === 'ios' ? 'simple_push' : 'fade_from_bottom',
+							animation: Platform.OS === "ios" ? "simple_push" : "fade_from_bottom",
 							gestureDirection: "horizontal",
 							gestureEnabled: true,
 							fullScreenGestureEnabled: true,
@@ -65,8 +90,13 @@ export default function RootLayout() {
 												<Text className="text-lg font-bold">Sponsors</Text>
 											</View>
 											<View className="flex-row items-center gap-4">
-												<TextInput autoCorrect={false} autoCapitalize="none" className="basis-8/12 rounded-xl bg-gray-100 p-4" placeholder="Rechercher..." />
-												<Pressable className="bg-dark grow rounded-xl p-4">
+												<TextInput
+													autoCorrect={false}
+													autoCapitalize="none"
+													className="basis-8/12 rounded-xl bg-gray-100 p-4"
+													placeholder="Rechercher..."
+												/>
+												<Pressable className="grow rounded-xl bg-dark p-4">
 													<Text className="font-bold text-white">Catégories</Text>
 												</Pressable>
 											</View>
